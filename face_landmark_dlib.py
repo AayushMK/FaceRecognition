cbf0190afe9e502e5255d42fa931bd9a1e184884
Aayush Maharjan <mhrzn.aayush@gmail.com>
2020-11-10 19:34:23 +0545

    "tweaks"

--- face_landmark_dlib.py
+++ face_landmark_dlib.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 hog_face_detector = dlib.get_frontal_face_detector()
 face_landmark = dlib.shape_predictor(
-    "./data/n/shape_predictor_68_face_landmarks.dat")
+    "./data/shape_predictor_68_face_landmarks.dat")
 
 
 def draw_landmark(img):
@@ -28,7 +28,7 @@ cap = cv2.VideoCapture(0)
 
 while True:
     rect, frame = cap.read(0)
-    f = draw_landmark(frame)
+    f = draw_landmark(cv2.flip(frame, 1))
     cv2.imshow('Video Face Detect', f)
     k = cv2.waitKey(1)
     if k == 27:
