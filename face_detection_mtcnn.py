b0f95d1edf3226d66f9baaa2e585b8547467bdb0
Aayush Maharjan <mhrzn.aayush@gmail.com>
2020-11-10 20:00:09 +0545

    Formal

--- face_detection_mtcnn.py
+++ face_detection_mtcnn.py
@@ -1,44 +1,48 @@
+# This program straightens the faces in the image, extracts them and saves them.
+# MTCNN is used for detecting detect faces
+
 from mtcnn import MTCNN
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-img = cv2.cvtColor(cv2.imread("./data/david.jpg"), cv2.COLOR_BGR2RGB)
-img_copy = img.copy()
+
 detector = MTCNN()
-x = detector.detect_faces(img)
 
-start = (x[0]['box'][0], x[0]['box'][1])
-end = (x[0]['box'][0]+x[0]['box'][2], x[0]['box'][1] + x[0]['box'][3])
-color = (255, 255, 255)
-cv2.rectangle(img, start, end, color, 10)
 
-p1 = x[0]['keypoints']['left_eye']
-p2 = x[0]['keypoints']['right_eye']
-print(p1)
-print(p2)
-cv2.circle(img, p1, 20, (255, 255, 0), -1)
-cv2.circle(img, p2, 20, (255, 255, 0), -1)
-plt.imshow(img)
+def detect_faces(img):
+    faces = detector.detect_faces(img)
+
+    for x in faces:
+        start = (x['box'][0], x['box'][1])
+        end = (x['box'][0]+x['box'][2], x['box'][1] + x['box'][3])
+        color = (255, 255, 255)
+        cv2.rectangle(img, start, end, color, 10)
+
+        p1 = x['keypoints']['left_eye']
+        p2 = x['keypoints']['right_eye']
+
+        cv2.circle(img, p1, 20, (255, 255, 0), -1)
+        cv2.circle(img, p2, 20, (255, 255, 0), -1)
+
+    return img
+
+
+img = cv2.cvtColor(cv2.imread("./data/david.jpg"), cv2.COLOR_BGR2RGB)
+detected_img = detect_faces(img)
+plt.imshow(detected_img)
 plt.show()
-# cv2.imshow("output", img)
-# cv2.waitKey(0)
 
+cap = cv2.VideoCapture(0)
 
-def getAngle(p1, p2):
+while True:
+    rect, frame = cap.read(0)
+    frame = detect_faces(cv2.flip(frame, 1))
+    cv2.imshow('Video Face Detect', frame)
+    k = cv2.waitKey(1)
+    if k == 27:
+        break
 
-    slope = (p2[1]-p1[1])/(p2[0]-p1[0])
-    angle = math.atan(slope)*(180/3.14)
-    print(angle)
-    return angle
-
-
-angle = getAngle(p1, p2)
-img2 = img_copy
-
-matrix = cv2.getRotationMatrix2D(p1, angle, 1)
-new_image = cv2.warpAffine(img2, matrix, (img2.shape[1], img2.shape[0]))
-cv2.rectangle(new_image, start, end, color, 10)
-plt.imshow(new_image)
-plt.show()
+cap.release()
+cv2.destroyAllWindows()
