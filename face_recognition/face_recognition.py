1b98f1065b8a6c722784c7f3eb458698e34014ab
Aayush Maharjan <mhrzn.aayush@gmail.com>
2020-11-19 13:40:08 +0545

    Using my own alignment function one

--- face_recognition/face_recognition.py
+++ face_recognition/face_recognition.py
@@ -4,33 +4,41 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 import argparse
+import face_extraction_dlib as fe
+
 
 class FaceRecognition:
+    #initialize required parameters
     def __init__(self, shape_predictor, model):
         self.shape_predictor = shape_predictor
+        self.detector = dlib.get_frontal_face_detector()
         self.model = model
         self.class_names = []
         self.encoding_known_list = []
-        
+    
+    #helper method to find distance between two encodings
     def find_euclidean_distance(self,source_representation, test_representation):
         euclidean_distance = source_representation - test_representation
         euclidean_distance = np.sum(np.multiply(euclidean_distance, euclidean_distance))
         euclidean_distance = np.sqrt(euclidean_distance)
         return euclidean_distance
 
+    #method to compare all known faces with the given images encodings 
     def face_distance(self ,encoding_face):
         distance = []
         for encoding in self.encoding_known_list:
             distance.append(self.find_euclidean_distance(encoding, encoding_face))
         return distance
 
+    #method that generates boolean list containing the 
     def compare_faces(self, encoding_face):
         comp = []
         distance = self.face_distance( encoding_face)
         for d in distance:
             comp.append(not (d > 0.6))
         return comp
-        
+
+    #encode known faces
     def encodings_known_img(self,path="KnownFaces"):
         images = []
         mylist = os.listdir(path)
@@ -40,38 +48,28 @@ class FaceRecognition:
             img = dlib.load_rgb_image(f'{path}/{myclass}')
             images.append(img)
             self.class_names.append(os.path.splitext(myclass)[0])
-
         encoding_list = []
-        for img in images:
-            img_detect = detector(img, 1)
-            
-            img_shape = self.shape_predictor(img, img_detect[0])
-            
-            img_chip = dlib.get_face_chip(img, img_shape)
+        
+        for img in images:    
+            img_detect = fe.detect_faces(img, self.detector, self.shape_predictor)
+            img_chip = img_detect[0][0]
             img_features = np.array(self.model.compute_face_descriptor(img_chip))
             encoding_list.append(img_features)
         self.encoding_known_list = encoding_list
 
-
+    #uses all the above functions to get the match and add bounding boxes and names
     def face_recognition(self,img, scale_factor):
         img_small = cv2.resize(img,(0,0),None,scale_factor,scale_factor)
-        img_small = cv2.cvtColor(img_small,cv2.COLOR_BGR2RGB)
-        img_detect = detector(img_small, 1)
+        img_detect = fe.detect_faces(img_small, self.detector, self.shape_predictor)
         left, right, top , bottom = 0,0,0,0
-        cf_loacations = []
-        cf_encoding = []
-        for k , d in enumerate(img_detect ):
-            left = d.left()
-            right = d.right()
-            top = d.top()
-            bottom = d.bottom()
-            cf_loacations.append(d)
-        
-            img_shape = self.shape_predictor(img_small, d)
-            img_chip = dlib.get_face_chip(img_small, img_shape)
-            img_features = np.array(self.model.compute_face_descriptor(img_chip))
+        cf_loacations = img_detect[2]
+        cf_encoding =[]
+
+        for i in img_detect[0]:
+            print(i.shape)
+            img_features = np.array(self.model.compute_face_descriptor(i))
             cf_encoding.append(img_features)
-        
+
         rescale_factor = 1/scale_factor
         for encode_face , face_location in zip(cf_encoding, cf_loacations):
             face_dis = self.face_distance(encode_face)
@@ -90,7 +88,8 @@ class FaceRecognition:
                 cv2.rectangle(img, (left, bottom), (right, bottom+35), (255,0,0), cv2.FILLED)
                 cv2.putText(img, name,(left+6, bottom+30), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255, 255),2)
         return img
-
+        
+    #takes image as input and calls face_recognition method by passing image
     def show_image(self, path):
         img = cv2.imread(path)
         img = self.face_recognition(img, 2)
@@ -98,6 +97,7 @@ class FaceRecognition:
         plt.imshow(img)
         plt.show()
 
+    #takes video as input and calls face_recognition method on each frame
     def show_video(self, video):
         cap = cv2.VideoCapture(video)
 
@@ -122,10 +122,9 @@ if __name__ == "__main__":
     parser.add_argument("-ki","--known_images", type=str, help="path of folder containg known images for training/extracting encodings")
     args = parser.parse_args()
 
-
-    detector = dlib.get_frontal_face_detector()
+    
     if(args.shape_predictor == None):
-        shape_predictor = dlib.shape_predictor("../data/shape_predictor_5_face_landmarks.dat/shape_predictor_5_face_landmarks.dat")
+        shape_predictor = dlib.shape_predictor("../data/shape_predictor_68_face_landmarks.dat")
     else:
         shape_predictor = dlib.shape_predictor(args.shape_predictor)
     if(args.model == None):
@@ -133,20 +132,24 @@ if __name__ == "__main__":
     else:
         model = dlib.face_recognition_model_v1(args.model)
     
-    frc = FaceRecognition(shape_predictor, model)
+    #Get FaceRecognition object
+    frc = FaceRecognition( shape_predictor, model)
+
     if(args.known_images == None):
         frc.encodings_known_img()
     else:
         frc.encodings_known_img(args.known_images)
     print(frc.class_names)
 
-
-    if args.ivpath.endswith(".jpg") or args.ivpath.endswith(".png") or args.ivpath.endswith(".jpeg"):
-        frc.show_image(args.ivpath)
-    elif args.ivpath.endswith(".mp4"):
-        frc.show_video(args.ivpath)
-    elif args.ivpath == '0':
-        frc.show_video(0)
+    if(args.ivpath == None):
+        print("File path not specified see help using -h")
     else:
-        print("File format not supported")
+        if args.ivpath.endswith(".jpg") or args.ivpath.endswith(".png") or args.ivpath.endswith(".jpeg"):
+            frc.show_image(args.ivpath)
+        elif args.ivpath.endswith(".mp4"):
+            frc.show_video(args.ivpath)
+        elif args.ivpath == '0':
+            frc.show_video(0)
+        else:
+            print("File format not supported")
     
