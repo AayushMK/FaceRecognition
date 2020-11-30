d03176a64c52e58c6c62b5f07e6bf24517d16173
Aayush Maharjan <mhrzn.aayush@gmail.com>
2020-11-30 12:05:26 +0545

    testing vgg model using lfw dataset. Changed to class

--- face_recognition_soa/test_vgg.py
+++ face_recognition_soa/test_vgg.py
@@ -13,87 +13,103 @@ from PIL import Image
 from matplotlib import pyplot
 import cv2
 
+class evluate_model():
+    def __init__(self):
+        self.flp = fetch_lfw_pairs(subset='test', color=True, resize=1  # this transform inputs to (125, 94) from (62, 47)
+                                        )
 
-def get_embeddings(samples):
+#helper fucntion to get embeddings of list of images
+    def get_embeddings(self, samples):
 
-    # prepare the face for the model, e.g. center pixels
-    samples = preprocess_input(samples, version=2)
-    # create a vggface model
-    model = VGGFace(model='resnet50', include_top=False,
-                    input_shape=(224, 224, 3), pooling='avg')
-    # perform prediction
+        # prepare the face for the model, e.g. center pixels
+        samples = preprocess_input(samples, version=2)
+        # create a vggface model
+        model = VGGFace(model='resnet50', include_top=False,
+                        input_shape=(224, 224, 3), pooling='avg')
+        # perform prediction
 
-    yhat = model.predict(samples)
-    return yhat
+        yhat = model.predict(samples)
+        return yhat
 
+#helper function to calculate distance and predict
+    def is_match(self, known_embedding, candidate_embedding, thresh=0.5):
+        # calculate distance between embeddings
+        score = cosine(known_embedding, candidate_embedding)
+        obj = {}
+        obj["distance"] = score
+        if score <= thresh:
+            obj["verified"] = True
+        else:
+            obj["verified"] = False
+        return obj
 
-def is_match(known_embedding, candidate_embedding, thresh=0.5):
-    # calculate distance between embeddings
-    score = cosine(known_embedding, candidate_embedding)
-    obj = {}
-    obj["distance"] = score
-    if score <= thresh:
-        obj["verified"] = True
-    else:
-        obj["verified"] = False
-    return obj
+#makes images ready for evaluation
+    def preprocess_data(self):
+        pairs = self.flp.pairs
+        instances = pairs.shape[0]
+        pairs_first_column = pairs[:, 0]
+        pairs_second_column = pairs[:, 1]
+        all_faces = []
+        print("resizing")
+        for i in range(0, 1000):
+            all_faces.append(cv2.resize(
+                pairs_first_column[i], (224, 224), interpolation=cv2.INTER_AREA))
+        for i in range(0, 1000):
+            all_faces.append(cv2.resize(
+                pairs_second_column[i], (224, 224), interpolation=cv2.INTER_AREA))
+        print("resiziing finished")
+        return all_faces
 
+#get embeddings and prediction for all the faces 
+    def evaluate(self, all_faces):
+        actuals = []
+        predictions = []
+        distances = []
 
-fetch_lfw_pairs = fetch_lfw_pairs(subset='test', color=True, resize=1  # this transform inputs to (125, 94) from (62, 47)
-                                  )
-pairs = fetch_lfw_pairs.pairs
-labels = fetch_lfw_pairs.target
-target_names = fetch_lfw_pairs.target_names
-target_names
+        print("embedding started")
+        embeddings = self.get_embeddings(all_faces)
+        print("embedding finished")
 
-instances = pairs.shape[0]
-actuals = []
-predictions = []
-distances = []
-pairs_first_column = pairs[:, 0]
-pairs_second_column = pairs[:, 1]
-all_faces = []
-print("resizing")
-for i in range(0, 1000):
-    all_faces.append(cv2.resize(
-        pairs_first_column[i], (224, 224), interpolation=cv2.INTER_AREA))
-for i in range(0, 1000):
-    all_faces.append(cv2.resize(
-        pairs_second_column[i], (224, 224), interpolation=cv2.INTER_AREA))
-print("resiziing finished")
+        embeddings_first = embeddings[0:1000]
+        embeddings_second = embeddings[1000:2000]
 
-print("embedding started")
-embeddings = get_embeddings(all_faces)
-print("embedding finished")
-embeddings_first = embeddings[0:1000]
-embeddings_second = embeddings[1000:2000]
-for i in range(1000):
-    face = []
-    pair = pairs[i]
+        labels = self.flp.target
+        target_names = self.flp.target_names
 
-    obj = is_match(embeddings_first[i], embeddings_second[i], thresh=0.5)
-    prediction = obj["verified"]
-    predictions.append(prediction)
+        for i in range(1000):
+            face = []
+            pair = pairs[i]
+            obj = is_match(self, embeddings_first[i], embeddings_second[i], thresh=0.5)
+            prediction = obj["verified"]
+            predictions.append(prediction)
 
-    distances.append(obj["distance"])
+            distances.append(obj["distance"])
 
-    label = target_names[labels[i]]
-    actual = True if labels[i] == 1 else False
-    actuals.append(actual)
+            label = target_names[labels[i]]
+            actual = True if labels[i] == 1 else False
+            actuals.append(actual)
+        return actuals, predictions
 
+#cacluates different metrics
+    def show_metric(self, actuals, predictions):
+        accuracy = 100*accuracy_score(actuals, predictions)
+        precision = 100*precision_score(actuals, predictions)
+        recall = 100*recall_score(actuals, predictions)
+        f1 = 100*f1_score(actuals, predictions)
 
-accuracy = 100*accuracy_score(actuals, predictions)
-precision = 100*precision_score(actuals, predictions)
-recall = 100*recall_score(actuals, predictions)
-f1 = 100*f1_score(actuals, predictions)
+        print("instances: ", len(actuals))
+        print("accuracy: ", accuracy, "%")
+        print("precision: ", precision, "%")
+        print("recall: ", recall, "%")
+        print("f1: ", f1, "%")
 
-print("instances: ", len(actuals))
-print("accuracy: ", accuracy, "%")
-print("precision: ", precision, "%")
-print("recall: ", recall, "%")
-print("f1: ", f1, "%")
+        cm = confusion_matrix(actuals, predictions)
 
-cm = confusion_matrix(actuals, predictions)
+        tn, fp, fn, tp = cm.ravel()
+        print((tn, fp, fn, tp))
 
-tn, fp, fn, tp = cm.ravel()
-print((tn, fp, fn, tp))
+if __name__ == "__main__":
+    em = evluate_model()
+    all_faces = em.preprocess_data()
+    actuals, predictions = em.evaluate(all_faces)
+    em.show_metric(actuals, predictions)
